a8a860545da478d87018d86f8af0890349d55050
unknown <fiwong2@697-334-Gollum.sf.k12.internal>
2016-01-21 11:05:06 -0800

    done

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -17,16 +17,16 @@ public class GoogleBillboard extends PApplet {
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
-     noLoop();
-     for (int i = 0; i < e.length(); i++)  
-     {
-         String digits = e.substring(i, i+10);  
-         double dNum = Double.parseDouble(digits);
-         if (isPrime(dNum) == true)
-         {
+    noLoop();
+    for (int i = 2; i < e.length(); i++)  
+    {
+        String digits = e.substring(i, i+10);  
+        double dNum = Double.parseDouble(digits);
+        if (isPrime(dNum) == true)
+        {
             System.out.println(dNum);
             break;
-          }
+        }
      }
 }  
 public void draw()  
@@ -37,7 +37,7 @@ public boolean isPrime(double dNum)
 {   
     for (int i = 2; i <= Math.sqrt(dNum); i++)
 	{
-		if (dNum % 1 == 0)
+		if (dNum % i == 0)
 			return false;
 	}
 	if (dNum <= 1)
